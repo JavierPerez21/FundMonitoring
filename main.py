dde5c840bfc2a00a415ea3de511534c7b30b512f
JavierPerez21 <javierpereztobia@gmail.com>
2022-06-04 16:39:24 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from src.fund import *
+
+
+def get_funds(country = 'Spain', issuer_filter='Santander', limit_inception_date="01/01/2012"):
+    available_funds = investpy.get_funds(country=country)
+    time.sleep(1)
+    funds = {}
+    # Iterate over rows of available funds dataframe
+    for index, row in available_funds.iterrows():
+        # Condition
+        if issuer_filter in  row['issuer']:
+          try:
+            fund = Fund(row)
+            if pd.to_datetime(fund.inception_date) < pd.to_datetime(limit_inception_date):
+              fund.get_historical_data()
+              fund.calculate_metrics()
+              funds[fund.name] = fund
+              print(f'{round(100*index/len(available_funds), 2)}% extracted data for {row["name"]}')
+              time.sleep(1)
+          except Exception:
+            pass
+
+def compare_funds(fund1=None, fund2=None,from_date="01/01/1990"):
+    available_funds = investpy.get_funds(country='Spain')
+    if fund1 is None:
+        fund1 = input("Enter first fund name: ")
+    if fund2 is None:
+        fund2 = input("Enter second fund name: ")
+    fund1 = Fund(available_funds.loc[available_funds[available_funds['name'] == fund1].index[0]])
+    fund2 = Fund(available_funds.loc[available_funds[available_funds['name'] == fund2].index[0]])
+
+    fund1.get_historical_data(from_date=from_date)
+    fund2.get_historical_data(from_date=from_date)
+
+    fig, ax = plt.subplots(figsize=(15, 8))
+    p1, = ax.plot(fund1.df['daily'][fund1.name], label=fund1.name, color='blue')
+    twin = ax.twinx()
+    p2, = twin.plot(fund2.df['daily'][fund2.name], label=fund2.name, color='red')
+    ax.set_title(f'Comparison of {fund1.name} and {fund2.name}')
+    ax.set_ylabel(fund1.name)
+    twin.set_ylabel(fund2.name)
+    ax.yaxis.label.set_color(p1.get_color())
+    twin.yaxis.label.set_color(p2.get_color())
+    plt.show()
+
+
+if __name__ == '__main__':
+    f1 = 'Santander Acciones Españolas A Fi'
+    compare_funds(fund1=f1, fund2='Morgan Stanley Investment Funds - Us Growth Fund A', from_date="01/01/1990")
+    compare_funds(fund1=f1, fund2='Blackrock Global Funds - Global Allocation Fund E2', from_date="01/01/1990")
+    compare_funds(fund1=f1, fund2='Blackrock Global Funds - Next Generation Technology Fund E2', from_date="01/01/1990")
---
+++ src/fund.py
@@ -0,0 +1,102 @@
+from .utils import *
+
+
+class Fund():
+    def __init__(self, fund_data):
+        self.save_info(fund_data)
+
+    def save_info(self, fund_data):
+        self.country = fund_data['country']
+        self.name = fund_data['name']
+        self.symbol = fund_data['symbol']
+        self.issuer = fund_data['issuer']
+        self.isin = fund_data['isin']
+        self.asset_class = fund_data['asset_class']
+        self.currency = fund_data['currency']
+        fund_info = investpy.funds.get_fund_information(self.name, self.country, as_json=False)
+        self.inception_date = fund_info['Inception Date'][0]
+        self.market_cap = fund_info['Market Cap'][0]
+        self.total_assets = fund_info['Total Assets'][0]
+        self.expenses = fund_info['Expenses'][0]
+        self.category = fund_info['Category'][0]
+
+    def get_historical_data(self, from_date="01/01/1990"):
+        to_date = datetime.datetime.now().strftime("%d/%m/%Y")
+        daily_df = investpy.get_fund_historical_data(fund=self.name, country=self.country, from_date=from_date, to_date=to_date).rename(columns={'Close': self.name})
+        daily_df['return'] = daily_df[self.name].pct_change()
+        daily_df = daily_df[abs(daily_df['return']) < 10*abs(daily_df['return']).median()]
+        yearly_df = daily_df.resample('A').last()
+        monthly_df = daily_df.resample('M').last()
+        self.df = {'daily': daily_df, 'yearly': yearly_df, 'monthly': monthly_df}
+        for period in self.df.keys():
+            self.df[period]['return'] = self.df[period][self.name].pct_change()
+        for window in [5, 10, 25, 50, 100, 150, 200, 300]:
+            self.df['daily']['EMA-' + str(window)] = ta.trend.ema_indicator(self.df['daily'][self.name], window=window)
+
+    def calculate_metrics(self):
+        daily = self.df['daily'][self.name]
+        self.inception_return = calculate_percent_return(daily)
+        self.years = get_years_past(daily)
+        self.cagr = calculate_cagr(daily)
+        self.annualized_volatility = calculate_annualized_volatility(daily)
+        self.sharpe_ratio = calculate_sharpe_ratio(daily)
+        self.rolling_sharpe_ratio = calculate_rolling_sharpe_ratio(daily)
+        self.annualized_downside_deviation = calculate_annualized_downside_deviation(daily)
+        self.sortino_ratio = calculate_sortino_ratio(daily)
+        self.pure_profit_score = calculate_pure_profit_score(daily)
+        #self.jensens_alpha = calculate_jensens_alpha(daily)
+        self.max_drawdown = calculate_max_drawdown(daily)
+        self.calmar_ratio = calculate_calmar_ratio(daily)
+
+    def to_dict(self):
+        info_dict = dict(
+            name=self.name,
+            issuer=self.issuer,
+            isin=self.isin,
+            asset_class=self.asset_class,
+            currency=self.currency,
+            inception_date=self.inception_date,
+            market_cap=self.market_cap,
+            total_assets=self.total_assets,
+            expenses=self.expenses,
+            category=self.category,
+            country=self.country,
+            inception_return=self.inception_return,
+            years=self.years,
+            cagr=self.cagr,
+            annualized_volatility=self.annualized_volatility,
+            sharpe_ratio=self.sharpe_ratio,
+            sortino_ratio=self.sortino_ratio,
+            pure_profit_score=self.pure_profit_score,
+            max_drawdown=self.max_drawdown,
+            calmar_ratio=self.calmar_ratio,
+        )
+        for y in [1, 3, 5, 10, 15, 20]:
+            info_dict['yearly_return_' + str(y)] = self.df['yearly'][self.name].pct_change(y).iloc[-1] ** (1/y)
+        for m in [1, 3, 6, 12, 18, 24]:
+            info_dict['monthly_return_' + str(m)] = self.df['monthly'][self.name].pct_change(m).iloc[-1] ** (1/m)
+        return info_dict
+
+    def display(self):
+        fig, (ax, tb) = plt.subplots(1, 2, figsize=(30, 10),gridspec_kw=dict(width_ratios=[2,1]))
+        ax.plot(self.df['daily'][self.name])
+        ax.set_title(self.name)
+        ax.grid(True)
+        info_dict = self.to_dict()
+        for info in info_dict:
+          if isinstance(info_dict[info], float):
+            if 'return' in info or info in ['max_drawdown', 'cagr']:
+              info_dict[info] = info_dict[info] * 100
+            if abs(info_dict[info]) > 1:
+              info_dict[info] = round(info_dict[info], 2)
+            else:
+              info_dict[info] = round(info_dict[info], 4)
+          if 'return' in info or info in ['max_drawdown', 'cagr', 'inception_return']:
+              info_dict[info] = f'{info_dict[info]}%'
+        txt = [[info, info_dict[info]] for info in info_dict.keys()]
+        # Display all information in dictionary info_dict in a table in tb (round all floats to 4 significant figures)
+        table = tb.table(cellText=txt, loc='center')
+        table.auto_set_font_size(False)
+        table.set_fontsize(12)
+        fig.tight_layout()
+        plt.show()
